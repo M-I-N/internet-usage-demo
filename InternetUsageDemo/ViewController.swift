38a2bde91e60bfa83363a49a332619d18cb0dcff
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2018-01-29 10:50:38 +0600

    Initial Commit

---
+++ InternetUsageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InternetUsageDemo
+//
+//  Created by Nayem BJIT on 1/29/18.
+//  Copyright © 2018 BJIT Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
